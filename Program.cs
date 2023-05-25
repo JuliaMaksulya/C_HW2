6da4b7efec7bda39f544bdb5bc06fca6e689ef5b
Julia <maksulyax^2gmail.com>
2023-05-25 19:22:51 +0300

    HW

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// Console.WriteLine("Ведите число: ");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+// int num2;
+
+// if (num > 99 && num < 999)
+// {
+
+//   num2 = num%100;
+//   num2 = num2/10;
+//   Console.WriteLine(num2);
+// }
+
+// else 
+// {
+//     Console.WriteLine("Введено не трехзначное число");
+// }
+
+
+
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// Console.WriteLine("Ведите число: ");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+// int num2;
+// if (num > 999 && num < 9999)
+// {
+//   num2 = num%100;
+//   num2 = num2/10;
+//   Console.WriteLine(num2);
+
+// }
+// else if (num > 99 && num < 999)
+// {
+//     num2 = num%10;
+//     Console.WriteLine(num2);
+// }
+
+// else if (num > 9999 && num < 99999)
+// {
+//     num2 = num%1000;
+//     num2 = num2/10;
+//     num2 = num2/10;
+//     Console.WriteLine(num2);
+// }
+
+// else 
+
+// Console.WriteLine("Третьей цифры нет");
+
+
+
+    
+
+
+
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+// Console.WriteLine("Ведите число от 1 до 7: ");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+
+
+// if (num <=5)
+// {
+//  Console.WriteLine("Идем на работу");
+// }
+
+// else 
+// {
+//     Console.WriteLine("Ура, сегодня выходной");
+// }
